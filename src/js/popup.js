55c412bceaf6dca7e3a5d522f43c68e695d9883d
Viktor Kirillov <mr.kirillov@icloud.com>
2019-01-05 21:01:58 +0200

    Initial commit

---
+++ src/js/popup.js
@@ -0,0 +1,10 @@
+import "../css/popup.css";
+import React from "react";
+import { render } from "react-dom";
+
+import App from './App';
+
+render(
+  <App/>,
+  window.document.getElementById("app-container")
+);
